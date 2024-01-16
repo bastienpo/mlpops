8c44793873fabb69b30ca956bc1a0ec118dfef2a
bastien <bastien.pouessel@epita.fr>
2024-01-16 21:55:37 +0100

    add producer and consumer with milvus

---
+++ data-ingestion/consumer/consumer.py
@@ -0,0 +1,47 @@
+from pulsar import Client
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Milvus
+
+PULSAR_HOST = "pulsar://localhost:6650"
+MILVUS_HOST = "localhost"
+MILVUS_PORT = 19530
+
+if __name__ == "__main__":
+    client = Client(PULSAR_HOST)
+    consumer = client.subscribe("topic_retrival", "sub_retrival")
+
+    model_name = "sentence-transformers/all-mpnet-base-v2"
+    model_kwargs = {'device': 'cpu'}
+    encode_kwargs = {'normalize_embeddings': False}
+    
+    embeddings = HuggingFaceEmbeddings(
+        model_name=model_name,
+        model_kwargs=model_kwargs,
+        encode_kwargs=encode_kwargs
+    )
+
+    milvus_store = Milvus(
+        embedding_function = embeddings,
+        collection_name = "LangChainCollection",
+        connection_args={"host": MILVUS_HOST, "port": MILVUS_PORT},
+        drop_old = True,
+    )
+
+    while True:
+        msg = consumer.receive()
+        try:
+            decoded_str = msg.data().decode('utf-8')
+
+            milvus_store.add_texts(
+                [decoded_str]
+            )
+            
+            print(f"Added text to Milvus: {msg.message_id()}")
+
+            # Acknowledge successful processing of message
+            consumer.acknowledge(msg)
+        except Exception:
+            consumer.negative_acknowledge(msg)
+        
+    client.close()
+
---
+++ data-ingestion/producer/producer.py
@@ -0,0 +1,37 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import WebBaseLoader
+from pulsar import Client
+
+PULSAR_HOST = "pulsar://localhost:6650"
+
+if __name__ == "__main__":
+    loader = WebBaseLoader(
+        [
+            "https://fr.wikipedia.org/wiki/Google",
+        ]
+    )
+
+    docs = loader.load()
+    splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=0)
+    splits = splitter.split_documents(docs)
+
+    client = Client(PULSAR_HOST)
+    producer = client.create_producer("topic_retrival")
+
+    for split in splits:
+        try:
+            producer.send(split.page_content.encode("utf-8"), None)
+        except Exception as e:
+            print(e)
+
+    producer.flush()
+    producer.close()
+
+# embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+# vector_db = Milvus.from_documents(
+#     documents=splits,
+#     embedding=embeddings,
+#     connection_args={"host": MILVUS_HOST, "port": MILVUS_PORT},
+#     collection_name="collection_retrival",
+# )
